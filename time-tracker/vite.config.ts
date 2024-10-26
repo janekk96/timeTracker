5e6eb2bae416186ceac93a0f784ae574513c8fe4
jszymczak <jan.szymczak@pergamonmed.com>
2024-10-26 18:32:10 +0200

    config update

--- time-tracker/vite.config.ts
+++ time-tracker/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  define: {
+    "process.env": process.env,
+    VITE_SUPABASE_PROJECT_URL: process.env.VITE_SUPABASE_PROJECT_URL,
+    VITE_SUPABASE_ANON_KEY: process.env.VITE_SUPABASE_ANON_KEY,
+  },
+});
