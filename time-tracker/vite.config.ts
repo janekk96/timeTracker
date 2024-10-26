e763b445f253ddb452f05c430883676199f1a389
jszymczak <jan.szymczak@pergamonmed.com>
2024-10-26 20:10:28 +0200

    pwa support added

--- time-tracker/vite.config.ts
+++ time-tracker/vite.config.ts
@@ -1,9 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestForPWA: Partial<VitePWAOptions> = {
+  registerType: "autoUpdate",
+  injectRegister: "auto",
+  includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+  devOptions: {
+    enabled: true,
+  },
+  workbox: {
+    globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+  },
+  manifest: {
+    name: "Czas pracy Forrent",
+    short_name: "CzasPracy",
+    description: "Aplikacja do zarządzania czasem pracy",
+    theme_color: "#ffffff",
+    icons: [
+      {
+        src: "logo192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "logo512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+    ],
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPWA)],
   // define: {
   //   "process.env": process.env,
   //   VITE_SUPABASE_PROJECT_URL: process.env.VITE_SUPABASE_PROJECT_URL,
